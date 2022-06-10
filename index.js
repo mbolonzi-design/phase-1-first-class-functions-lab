9307edf22d5f16e2e9125fe4f1fde84e5e4d2a4e
mbolonzi-design <mbolonzi.design@gmail.com>
2022-06-10 08:52:33 +0300

    commit tree 9234f33d66f038aaf7e9a7672b841fee16132908

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+//const drivers = ["Kioko", "John", "Carter", "Alice"]
+
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(fare){
+    return function(number){
+        return (fare * number)
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, diverToReturn){
+    return diverToReturn(drivers);
+}
